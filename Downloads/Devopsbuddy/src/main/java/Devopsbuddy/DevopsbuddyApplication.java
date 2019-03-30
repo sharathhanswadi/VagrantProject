b4f767402ffc7ac71de39f265b40e83205fb0232
sharath.hanswadi <shanswadi@gmail.com>
2019-03-30 10:16:35 +0530

    Added bootstarp  and jquery dependecies to pom

---
+++ Downloads/Devopsbuddy/src/main/java/Devopsbuddy/DevopsbuddyApplication.java
@@ -0,0 +1,13 @@
+package Devopsbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsbuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsbuddyApplication.class, args);
+	}
+
+}
